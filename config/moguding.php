e99dfb9211c0567cb12692353a6665fc46fafbe5
laradocs <75692074@qq.com>
2022-01-24 18:39:51 +0800

    更新配置文件

--- config/moguding.php
+++ config/moguding.php
@@ -1,9 +1,10 @@
 <?php
 
 return [
-    'device' => env ( 'MOGUDING_DEVICE', 'android' ),
+    'device' => env ( 'MOGUDING_DEVICE', '' ),
     'phone' => env ( 'MOGUDING_PHONE', '' ),
     'password' => env ( 'MOGUDING_PASSWORD', '' ),
+    'country' => env ( 'MOGUDING_COUNTRY', '中国' ),
     'province' => env ( 'MOGUDING_PROVINCE', '' ),
     'city' => env ( 'MOGUDING_CITY', '' ),
     'address' => env ( 'MOGUDING_ADDRESS', '' ),
