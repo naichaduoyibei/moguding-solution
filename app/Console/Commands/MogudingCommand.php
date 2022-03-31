661bcd8ce5be44a50a5972a06fd5ea738ddeac1c
laradocs <75692074@qq.com>
2022-04-01 00:08:46 +0800

    Fix bug

--- app/Console/Commands/MogudingCommand.php
+++ app/Console/Commands/MogudingCommand.php
@@ -5,8 +5,6 @@ namespace App\Console\Commands;
 use Exception;
 use Illuminate\Console\Command;
 use Laradocs\Moguding\Client;
-use Laradocs\Moguding\Exceptions\RequestTimeoutException;
-use Laradocs\Moguding\Exceptions\UnauthenticatedException;
 
 class MogudingCommand extends Command
 {
@@ -62,25 +60,25 @@ class MogudingCommand extends Command
 
         $plans ?: throw new Exception ('签到失败，你没有签到计划。');
 
-        $params = [
-            $user ['token'],
-            $user ['userId'],
-            config('moguding.province'),
-            config('moguding.city'),
-            config('moguding.address'),
-            config('moguding.longitude'),
-            config('moguding.latitude'),
-            config('moguding.type'),
-            config('moguding.device'),
-            $plan ['planId'],
-            config('moguding.description')
-        ];
-
         foreach ($plans as $plan) {
+            $params = [
+                $user ['token'],
+                $user ['userId'],
+                config('moguding.province'),
+                config('moguding.city'),
+                config('moguding.address'),
+                config('moguding.longitude'),
+                config('moguding.latitude'),
+                config('moguding.type'),
+                config('moguding.device'),
+                $plan ['planId'],
+                config('moguding.description')
+            ];
+
             $data = $factory->save(...$params);
 
             $data ?: throw new Exception ('考勤失败。如果你看见了这个，一定要及时提 Issues，这意味着代码需要更新了。');
-            
+
             $factory->sctSend(
                 config('moguding.sct.key'),
                 sprintf('%s %s %s', '蘑菇丁', ((config('moguding.type') === 'START') ? '上班' : '下班'), '打卡成功！'),
