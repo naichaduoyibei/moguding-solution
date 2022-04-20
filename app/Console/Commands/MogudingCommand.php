d9797da89ff8e384b1383e3843d24bc154b7563a
laradocs <75692074@qq.com>
2022-04-20 18:39:31 +0800

    修复打卡失败问题

--- app/Console/Commands/MogudingCommand.php
+++ app/Console/Commands/MogudingCommand.php
@@ -2,9 +2,13 @@
 
 namespace App\Console\Commands;
 
-use Exception;
 use Illuminate\Console\Command;
 use Laradocs\Moguding\Client;
+use Laradocs\Moguding\Exceptions\RequestTimeoutException;
+use Laradocs\Moguding\Exceptions\SendKeyInvalidException;
+use Laradocs\Moguding\Exceptions\UnauthenticatedException;
+use Laradocs\Moguding\Extensions\Notification;
+use Exception;
 
 class MogudingCommand extends Command
 {
@@ -13,7 +17,7 @@ class MogudingCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'moguding';
+    protected $signature = 'moguding {type?}';
 
     /**
      * The console command description.
@@ -22,13 +26,19 @@ class MogudingCommand extends Command
      */
     protected $description = '蘑菇丁自动签到';
 
+    protected Client $factory;
+
+    protected Notification $notification;
+
     /**
      * Create a new command instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Client $factory, Notification $notification)
     {
+        $this->factory = $factory;
+        $this->notification = $notification;
         parent::__construct();
     }
 
@@ -39,53 +49,95 @@ class MogudingCommand extends Command
      */
     public function handle()
     {
-        $factory = new Client();
-
-        $loginParams = [
+        $parameters = [
             config('moguding.device'),
             config('moguding.phone'),
-            config('moguding.password')
+            config('moguding.password'),
         ];
+        $user = $this->login(...$parameters);
+        unset($parameters);
 
-        $user = $factory->login(...$loginParams);
-
-
-        $getPlanParams = [
-            $user ['token'],
-            $user ['userType'],
-            $user ['userId']
+        $parameters = [
+            $user['token'],
+            $user['userType'],
+            $user['userId'],
         ];
-
-        $plans = $factory->getPlan(...$getPlanParams);
-
-        $plans ?: throw new Exception ('签到失败，你没有签到计划。');
+        $plans = $this->plans(...$parameters);
+        unset($parameters);
 
         foreach ($plans as $plan) {
-            $params = [
-                $user ['token'],
-                $user ['userId'],
+            $parameters = [
+                $user['token'],
+                $user['userId'],
                 config('moguding.province'),
                 config('moguding.city'),
                 config('moguding.address'),
                 config('moguding.longitude'),
                 config('moguding.latitude'),
-                config('moguding.type'),
+                $this->argument('type') ?? config('moguding.type'),
                 config('moguding.device'),
-                $plan ['planId'],
-                config('moguding.description')
+                $plan['planId'],
+                config('moguding.description'),
+                config('moguding.country'),
             ];
+            $data = $this->save(...$parameters);
+            unset($parameters);
+        }
+        $parameters = [
+            config('moguding.sct.key'),
+            "[{$data['createTime']}] 打卡成功",
+            config('moguding.country') . config('moguding.province') . config('moguding.city') . config('moguding.address')
+        ];
+        $this->sct(...$parameters);
+        unset($parameters);
+        $this->info("[{$data['createTime']}] 签到成功！");
+    }
 
-            $data = $factory->save(...$params);
+    protected function login(string $device, string $phone, string $password): array
+    {
+        try {
+            $user = $this->factory->login($device, $phone, $password);
+        } catch (RequestTimeoutException) {
+            $this->login($device, $phone, $password);
+        } catch (UnauthenticatedException $e) {
+            throw new Exception($e->getMessage());
+        }
 
-            $data ?: throw new Exception ('考勤失败。如果你看见了这个，一定要及时提 Issues，这意味着代码需要更新了。');
+        return $user;
+    }
 
-            $factory->sctSend(
-                config('moguding.sct.key'),
-                sprintf('%s %s %s', '蘑菇丁', ((config('moguding.type') === 'START') ? '上班' : '下班'), '打卡成功！'),
-                sprintf('%s%s%s', config('moguding.province'), (config('moguding.city') ?? ''), config('moguding.address'))
-            );
+    protected function plans(string $token, string $userType, int $userId): array
+    {
+        try {
+            $plans = $this->factory->getPlan($token, $userType, $userId);
+        } catch (RequestTimeoutException) {
+            $this->plans($token, $userType, $userId);
+        } catch (UnauthenticatedException $e) {
+            throw new Exception($e->getMessage());
+        }
 
-            $this->info('打卡成功！');
+        return $plans;
+    }
+
+    protected function save(string $token, int $userId, string $province, ?string $city, string $address, float $longitude, float $latitude, string $type, string $device, string $planId, ?string $description = null, string $country = '中国'): array
+    {
+        try {
+            $data = $this->factory->save($token, $userId, $province, $city, $address, $longitude, $latitude, $type, $device, $planId, $description, $country);
+        } catch (RequestTimeoutException) {
+            $this->save($token, $userId, $province, $city, $address, $longitude, $latitude, $type, $device, $planId, $description, $country);
+        } catch (UnauthenticatedException $e) {
+            throw new Exception($e->getMessage());
+        }
+
+        return $data;
+    }
+
+    protected function sct(?string $sendKey = null, ?string $title = null, ?string $desp = null): void
+    {
+        try {
+            $this->notification->sct($sendKey, $title, $desp);
+        } catch (SendKeyInvalidException) {
+            $this->warn('SendKey 配置错误。');
         }
     }
 }
